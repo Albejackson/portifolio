366d48fce1be7337656c2edb3c31340f9a630064
Albejackson <albejacksonmartins5@gmail.com>
2024-06-06 20:02:27 -0300

    portifolio

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuLinks = document.querySelectorAll('nav ul li a');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'nearest'
+            });
+        });
+    });
+});
